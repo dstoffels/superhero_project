b799c28b64ff632c39a26edcc91ce9c5a4361a77
dstoffels <dan.stoffels@gmail.com>
2022-01-19 14:17:40 -0600

    implemented superheroes/detail.html template

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -7,3 +7,11 @@ def index(request):
   all_heroes = Superhero.objects.all()
   context = { 'all_heroes': all_heroes }
   return render(request, 'superheroes/index.html', context)
+
+def detail(request, hero_id):
+  hero = Superhero.objects.get(pk=hero_id)
+  context = { 'hero': hero}
+  return render(request, 'superheroes/detail.html', context)
+
+def create(request):
+  return render(request, 'superheroes/create.html')
