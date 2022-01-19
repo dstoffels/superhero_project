dd184126261b018804971473b4d92653fe7c21af
dstoffels <dan.stoffels@gmail.com>
2022-01-19 13:51:26 -0600

    UPDATED: root-level routing, ADDED: superheroes template

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -1,3 +1,9 @@
+import imp
 from django.shortcuts import render
+from .models import Superhero
 
 # Create your views here.
+def index(request):
+  all_heroes = Superhero.objects.all()
+  context = { 'all_heroes': all_heroes }
+  return render(request, 'superheroes/index.html', context)
